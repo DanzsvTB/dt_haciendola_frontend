b34e7a77a61b7cebaae08a688b2e8d82f3934bdd
DanzsvTB <rogelio.sandoval@threebranches.io>
2024-05-14 23:20:38 -0500

    theme fix

--- src/contexts/color-mode/index.tsx
+++ src/contexts/color-mode/index.tsx
@@ -1,6 +1,6 @@
-import { RefineThemes } from "@refinedev/antd";
-import { ConfigProvider, theme } from "antd";
-import { PropsWithChildren, createContext, useEffect, useState } from "react";
+import { RefineThemes } from '@refinedev/antd';
+import { ConfigProvider, theme } from 'antd';
+import { PropsWithChildren, createContext, useEffect, useState } from 'react';
 
 type ColorModeContextType = {
   mode: string;
@@ -14,25 +14,25 @@ export const ColorModeContext = createContext<ColorModeContextType>(
 export const ColorModeContextProvider: React.FC<PropsWithChildren> = ({
   children,
 }) => {
-  const colorModeFromLocalStorage = localStorage.getItem("colorMode");
+  const colorModeFromLocalStorage = localStorage.getItem('colorMode');
   const isSystemPreferenceDark = window?.matchMedia(
-    "(prefers-color-scheme: dark)"
+    '(prefers-color-scheme: dark)'
   ).matches;
 
-  const systemPreference = isSystemPreferenceDark ? "dark" : "light";
+  const systemPreference = isSystemPreferenceDark ? 'dark' : 'light';
   const [mode, setMode] = useState(
     colorModeFromLocalStorage || systemPreference
   );
 
   useEffect(() => {
-    window.localStorage.setItem("colorMode", mode);
+    window.localStorage.setItem('colorMode', mode);
   }, [mode]);
 
   const setColorMode = () => {
-    if (mode === "light") {
-      setMode("dark");
+    if (mode === 'light') {
+      setMode('dark');
     } else {
-      setMode("light");
+      setMode('light');
     }
   };
 
@@ -46,10 +46,9 @@ export const ColorModeContextProvider: React.FC<PropsWithChildren> = ({
       }}
     >
       <ConfigProvider
-        // you can change the theme colors here. example: ...RefineThemes.Magenta,
         theme={{
-          ...RefineThemes.Blue,
-          algorithm: mode === "light" ? defaultAlgorithm : darkAlgorithm,
+          algorithm:
+            mode === 'light' ? theme.defaultAlgorithm : theme.darkAlgorithm,
         }}
       >
         {children}
